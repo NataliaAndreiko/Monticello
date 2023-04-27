c146d2136e62a04199dd67729cb220cd22573255
disiqui <ditriksy@gmail.com>
2023-04-27 21:05:05 +0300

    Add script for slider

---
+++ scripts/swipers.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".start-page__swiper", {
+    direction: "vertical",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 155000,
+    },
+    // loop: true,
+});
