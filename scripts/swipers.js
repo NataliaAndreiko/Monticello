efd27119da98dd02f4716d44d55a7b1899a47859
disiqui <ditriksy@gmail.com>
2023-04-28 22:58:52 +0300

    Add news section eith styles

--- scripts/swipers.js
+++ scripts/swipers.js
@@ -9,3 +9,20 @@ let swiper = new Swiper(".start-page__swiper", {
     },
     loop: true,
 });
+
+var swiper2 = new Swiper(".news__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
